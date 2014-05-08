a568b93cfe8a310b7ed7f6b92bceb28a78f5a472
Scott Opell <me@scottopell.com>
2014-05-08 08:49:27 -0400

    trying to follow best practices with rspec

--- spec/quadeight_spec.rb
+++ spec/quadeight_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Quadeight do
+  API_KEY = '8386c94e832b84206fd1adba087c3009284e62bb'
 
-  before(:all) do
-    api_key = '8386c94e832b84206fd1adba087c3009284e62bb'
-    @client = Quadeight.create_client api_key: api_key
+  let(:client) do
+    Quadeight.create_client api_key: API_KEY
   end
 
   it 'correctly authenticates' do
-    mixes = @client.get_mixes
+    mixes = client.get_mixes
     mixes.should_not eq('You must use a valid API key.')
   end
 
   it 'gets mixes correctly' do
-    @client.get_mixes
+    client.get_mixes
     mixes.each do |m|
       m.class.should eq(Mix)
     end
